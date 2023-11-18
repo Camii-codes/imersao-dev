686c22ff65fb52e64e873992c4688b38667e2e86
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-11-18 17:06:44 -0300

    update

---
+++ Conversor/bit.js
@@ -0,0 +1,6 @@
+let valorBitcoin = window.prompt('Qual o valor?');
+let cotacaoBitcoin = 180.177;
+
+valorReal = valorBitcoin * cotacaoBitcoin;
+
+window.alert('O valor em real é R$' + valorReal.toFixed(2));
--- Conversor/index.js
+++ Conversor/index.js
@@ -1,6 +1,7 @@
+let nome = window.prompt('Qual seu nome?');
 let valorDolar = window.prompt('Qual o valor?');
 let cotacaoDoDolar = 5.32;
 
 let valorReal = valorDolar * cotacaoDoDolar;
 
-window.alert("R$" + valorReal);
+window.alert(`Olá ${nome}, o valor em real é R$${valorReal.toFixed(2)}`);
