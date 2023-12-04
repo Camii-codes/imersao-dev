bb3a37e01255d66899445a69e0fc01a1c7e872de
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-04 15:50:19 -0300

    update

--- Conversor/anos.js
+++
@@ -1,6 +0,0 @@
-let anosLuz = window.prompt('Quantos anos luz?');
-let umAnoLuz = 9.461;
-
-let kiloMetros = anosLuz * umAnoLuz; 
-
-window.alert(`${anosLuz} anos luz, equivale a ${kiloMetros.toFixed(2)}Km`);
