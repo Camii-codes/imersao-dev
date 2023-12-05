4811b09153bf4695e238b950b5c96f5481996de5
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-05 09:25:13 -0300

    while

--- Jogo/index.js
+++ Jogo/index.js
@@ -1,12 +1,15 @@
 let numeroSecreto = 581;
-let chute = window.prompt('Digite um número de 1 a 1000');
+let chute;
 
-if (chute == numeroSecreto) {
-    window.alert('Acertou!');
-} else if (chute > numeroSecreto) {
-    window.alert('Errou... o número é menor.'); 
-} else if (chute < numeroSecreto) {
-    window.alert('Errou... o número é maior.');
+while(chute != numeroSecreto) {
+    let chute = window.prompt('Digite um número de 1 a 1000')
+    if (chute == numeroSecreto) {
+        window.alert('Acertou!');
+    } else if (chute > numeroSecreto) {
+        window.alert('Errou... o número é menor.'); 
+    } else if (chute < numeroSecreto) {
+        window.alert('Errou... o número é maior.');
+    }
 }
 
 
