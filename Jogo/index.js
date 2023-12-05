7e5e804af0e5cc8021a9e09c296f804cbb4a224a
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-05 08:39:33 -0300

    update

---
+++ Jogo/index.js
@@ -0,0 +1,10 @@
+let numeroSecreto = 581;
+let chute = window.prompt('Digite um número de 1 a 1000');
+
+if (chute == numeroSecreto) {
+    window.alert('Acertou!');
+} else {
+    window.alert('Errou!'); 
+}
+
+
