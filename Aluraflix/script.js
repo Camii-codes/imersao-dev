114e7c84991ec49041ee663dbefd98ee2baede64
Camila Andrade <camilaluanarodrigues9@gmail.com>
2023-12-21 17:12:45 -0300

    Finalizei projeto

--- Aluraflix/script.js
+++ Aluraflix/script.js
@@ -1,1 +1,34 @@
-// var listaFilmes = [?]
+let listaFilmes = []
+
+listaFilmes[0] = "https://upload.wikimedia.org/wikipedia/pt/5/56/Calif%C3%B3rnia_%28filme%29.jpg";
+listaFilmes[1] = "https://upload.wikimedia.org/wikipedia/pt/2/20/Submarine_%28filme%29.png";
+listaFilmes[2] = "https://br.web.img3.acsta.net/pictures/14/12/02/22/15/327211.jpg";
+listaFilmes[3] = "https://upload.wikimedia.org/wikipedia/pt/thumb/f/f6/Dark_%28s%C3%A9rie%29.jpg/300px-Dark_%28s%C3%A9rie%29.jpg";
+listaFilmes[4] = "https://cdn.fstatic.com/media/movies/covers/2017/10/MV5BNzI5OWQ5YTMtZWFkMi00Y2NhLWI1YjUtNGExOGMyZWIyNjNlXkEyXkFqcGdeQX_gppzMjA.jpg";
+listaFilmes[5] = "https://upload.wikimedia.org/wikipedia/pt/5/58/Donnie_Darko.jpg";
+listaFilmes[6] = "https://br.web.img3.acsta.net/pictures/14/10/28/21/28/104479.jpg";
+listaFilmes[7] = "https://br.web.img2.acsta.net/medias/nmedia/18/90/07/53/20391069.jpg";
+listaFilmes[8] = "https://br.web.img2.acsta.net/pictures/19/08/12/23/54/1624244.jpg";
+listaFilmes[9] = "https://br.web.img3.acsta.net/pictures/22/11/30/19/53/5856320.jpg";
+listaFilmes[10] = "https://upload.wikimedia.org/wikipedia/pt/9/9a/Big-bang-theory_Temporada_12_poster.jpg";
+listaFilmes[11] = "https://upload.wikimedia.org/wikipedia/pt/6/63/As-Vantagens-de-ser-Invisivel.jpg";
+listaFilmes[12] = "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg";
+listaFilmes[13] = "https://ideiasesquecidas.files.wordpress.com/2021/04/radioactive-2019-02.jpg"
+listaFilmes[14] = "https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg";
+listaFilmes[15] = "https://upload.wikimedia.org/wikipedia/pt/3/3a/Interstellar_Filme.png";
+listaFilmes[16] = "https://br.web.img3.acsta.net/pictures/14/02/04/13/20/128334.jpg";
+listaFilmes[17] = "https://m.media-amazon.com/images/I/41PEtSy5VsL.jpg";
+listaFilmes[18] = "https://upload.wikimedia.org/wikipedia/pt/6/63/Joker_%282019%29.jpg";
+listaFilmes[19] = "https://cinegarimpo.com.br/wp/content/uploads/2022/09/cinegarimpo.com.br-o-homem-ideal-ich-bin-dein-mensch-o-homem-ideal.jpeg";
+listaFilmes[20] = "https://m.media-amazon.com/images/I/81kL7SpTBjL._AC_UF1000,1000_QL80_.jpg";
+
+
+for (let i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">")
+}
+
+/*
+document.write("<img src=" + listaFilmes[0] + ">")
+document.write("<img src=" + listaFilmes[1] + ">")
+document.write("<img src=" + listaFilmes[2] + ">")
+*/
